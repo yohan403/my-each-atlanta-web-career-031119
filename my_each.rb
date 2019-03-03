b2c22a2363a4338f7a8e9840dd91b73a9fa8c708
John Cho <johnycho16@gmail.com>
2019-03-03 22:49:41 +0000

    Done.

--- my_each.rb
+++ my_each.rb
@@ -1,3 +1,13 @@
-def my_each # put argument(s) here
+def my_each(array)# put argument(s) here
   # code here
+  i = 0
+  while i < array.length
+    yield(array[i])
+    i = i + 1
+  end
+  array
+end
+
+my_each([1,2,3,4]) do |i|
+  i
 end
